322143b486e6411a8f774f112d0a54bf7b2c3758
Vidisha <biswalvidisha31@gmail.com>
2026-06-23 20:41:41 +0530

    added cros origin

--- backend/server.js
+++ backend/server.js
@@ -1,32 +1,40 @@
 require("dotenv").config();
 
-const express = require('express');
-const cors = require('cors');
-const path = require('path');  
+const express = require("express");
+const cors = require("cors");
 
-const connectDB = require('./config/db');
-const { errorHandler } = require('./middleware/errorMiddleware');
+const connectDB = require("./config/db");
+const { errorHandler } = require("./middleware/errorMiddleware");
+
 const app = express();
 
 connectDB();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://ai-task-manager-iv8k3eh30-work-lab.vercel.app"
+    ]
+  })
+);
+
 app.use(express.json());
 
- app.use("/api/auth", require('./routes/authRoutes'));
-app.use("/api/tasks", require('./routes/taskRoutes'));
-app.use("/api/ai", require('./routes/aiRoutes'));
+app.use("/api/auth", require("./routes/authRoutes"));
+app.use("/api/tasks", require("./routes/taskRoutes"));
+app.use("/api/ai", require("./routes/aiRoutes"));
 
- app.use(express.static(path.join(__dirname, '../frontend/dist')));
-
- app.get('/*splat', (req, res) => {
-    res.sendFile(path.join(__dirname, '../frontend/dist/index.html'));
+app.get("/", (req, res) => {
+  res.json({
+    message: "AI Task Manager API Running"
+  });
 });
 
- app.use(errorHandler);
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
